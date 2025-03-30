5bfc4c77772107f1bff5dc3551a96ef08ce471ea
Hajar El Moctar <125472535+HajarElMoctar@users.noreply.github.com>
2025-03-30 06:56:20 +0300

    Used executeQueryWithParams function in check_username.php

--- signup/check_username.php
+++ signup/check_username.php
@@ -5,11 +5,8 @@ $query = new Database();
 if (isset($_POST['username'])) {
     $username = $_POST['username'];
     
-    $stmt = $query->conn->prepare("SELECT * FROM accounts WHERE username = ?");
-    $stmt->bind_param("s", $username);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
+    $result = $query->executeQueryWithParams("SELECT * FROM accounts WHERE username = ?", [$username]);
+    
     if ($result->num_rows > 0) {
         echo 'exists';
     } else {
